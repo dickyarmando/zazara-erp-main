79633eba2172670dbafe2a5868aeefbff4424a00
dickyarmando <dicky.armando1306@gmail.com>
2025-01-03 11:07:36 +0700

    Update data-view-sales-non.blade.php
    QTY rata kanan

--- resources/views/livewire/sales/data-view-sales-non.blade.php
+++ resources/views/livewire/sales/data-view-sales-non.blade.php
@@ -43,7 +43,7 @@
                     </td>
                     <td class="px-2" style="border: 1px solid #000;">{{ $item['code'] }}</td>
                     <td class="px-2" style="border: 1px solid #000;">{{ $item['name'] }}</td>
-                    <td class="px-2 text-center" style="border: 1px solid #000;">
+                    <td class="px-2 text-right" style="border: 1px solid #000;">
                         {{ number_format($item['qty'], 0, ',', '.') }}</td>
                     <td class="px-2 text-center" style="border: 1px solid #000;">{{ $item['unit'] }}</td>
                     <td class="px-2 text-right" style="border: 1px solid #000;">
