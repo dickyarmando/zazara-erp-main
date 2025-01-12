3895119d344b543375fe89df736b0ba0537d5e60
kunclungg <35949350+kunclungg@users.noreply.github.com>
2025-01-12 10:56:51 +0700

    Feat : Delete table MsIntensiveUser add table MsInsentifSales

---
+++ database/migrations/2025_01_12_105519_create_ms_insentif_sales.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ms_insentif_sales', function (Blueprint $table) {
+            $table->id()->index();
+            $table->bigInteger('user_id')->index();
+            $table->decimal('target_amount', 18, 2)->default(0);
+            $table->integer('up')->default(0);
+            $table->integer('down')->default(0);
+            $table->tinyInteger('is_status')->default(1)->index();
+            $table->timestamps();
+            $table->bigInteger('created_by')->nullable()->index();
+            $table->bigInteger('updated_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ms_insentif_sales');
+    }
+};
