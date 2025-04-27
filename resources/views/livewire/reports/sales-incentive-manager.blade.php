89159c858cf36eaefb5998428381969008b15cae
polynomial232 <daffadhiya50@gmail.com>
2025-04-27 22:17:47 +0700

    feat: add sales ooption

--- resources/views/livewire/reports/sales-incentive-manager.blade.php
+++ resources/views/livewire/reports/sales-incentive-manager.blade.php
@@ -99,6 +99,7 @@
                             <div class="col-md-4">
                                 <div class="mb-3">
                                     <label class="form-label">Sales Username</label>
+                                    <option value="">Pilih Sales</option>
                                     <select class="form-control" wire:model="sales_username" placeholder="Sales Username">
                                         @foreach ($listSales as $sales)
                                             <option value="{{ $sales->username }}">
